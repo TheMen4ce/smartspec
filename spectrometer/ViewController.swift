6fe388ac5f5f4897c67acb57d3e4af77de40673c
Dennis Briner <dennis.briner@lightray.io>
2021-10-28 09:43:53 +0200

    Initial Commit

---
+++ spectrometer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  spectrometer
+//
+//  Created by Dennis Briner on 28.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
